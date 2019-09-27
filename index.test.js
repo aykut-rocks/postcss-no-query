03c31837e05f95b2c684cb79736e577e5bc4ccf8
Sensoy, Aykut <aykut.sensoy@spring-media.de>
2019-09-27 13:47:17 +0200

    added unit tests

--- index.test.js
+++ index.test.js
@@ -9,10 +9,42 @@ function run (input, output, opts) {
   })
 }
 
-/* Write tests here
-
-it('does something', function () {
-  return run('a{ }', 'a{ }', { })
+it('renders no-query fallback with default prefix and one media query expression in array', function () {
+  return run(
+    '@media (min-width: 600px) { .foo { border: 1px solid red; } } @media (min-width: 1024px) { .foo { border: 1px solid yellow; } }',
+    '@media (min-width: 600px) { .foo { border: 1px solid red; } } @media (min-width: 1024px) { .foo { border: 1px solid yellow; } } .no-query .foo { border: 1px solid yellow; }',
+    { query: ['(min-width: 1024px)'] }
+  )
 })
 
-*/
+it('renders no-query fallback with default prefix and multiple media query expressions in array', function () {
+  return run(
+    '@media (min-width: 600px) { .foo { border: 1px solid red; } } @media (min-width: 1024px) { .foo { border: 1px solid yellow; } }',
+    '@media (min-width: 600px) { .foo { border: 1px solid red; } } .no-query .foo { border: 1px solid red; } @media (min-width: 1024px) { .foo { border: 1px solid yellow; } } .no-query .foo { border: 1px solid yellow; }',
+    { query: ['(min-width: 600px)', '(min-width: 1024px)'] }
+  )
+})
+
+it('renders no-query fallback with custom prefix', function () {
+  return run(
+    '@media (min-width: 1024px) { .foo { border: 1px solid yellow; } }',
+    '@media (min-width: 1024px) { .foo { border: 1px solid yellow; } } .nobp .foo { border: 1px solid yellow; }',
+    { prefix: '.nobp', query: ['(min-width: 1024px)'] }
+  )
+})
+
+it('renders no no-query fallback if fallback is deactivated', function () {
+  return run(
+    '@media (min-width: 1024px) { .foo { border: 1px solid yellow; } }',
+    '@media (min-width: 1024px) { .foo { border: 1px solid yellow; } }',
+    { fallback: false, query: ['(min-width: 1024px)'] }
+  )
+})
+
+it('renders no no-query fallback if query array is empty', function () {
+  return run(
+    '@media (min-width: 1024px) { .foo { border: 1px solid yellow; } }',
+    '@media (min-width: 1024px) { .foo { border: 1px solid yellow; } }',
+    { query: [] }
+  )
+})
